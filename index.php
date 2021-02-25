5e6226524c95d43637c5356737881af84f048b88
JacobSeatlholo <66915934+JacobSeatlholo@users.noreply.github.com>
2021-02-25 13:15:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<style>
+body {
+  background-color: black;
+  text-align: center;
+  color: white;
+}
+</style>
+</head>
+<body>
 
-echo "hello world"
-  
->?
-  
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+<img src="avatar.png" alt="Avatar" style="width:200px">
+
+</body>
+</html>
